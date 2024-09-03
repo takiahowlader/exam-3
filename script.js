777d769a6ddb9a55f479b4fe6e6f54e843f23e04
takiahowlader <takiahowlader4466@gmail.com>
2024-09-03 21:20:03 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+
+function myFunction(){
+    document.getElementById("name").innerHTML = "Gently";
+}
+
+
+function change(){
+    document.getElementById('demo').innerHTML = "Plan";
+}
+
+
+
+function myimage() {
+    document.getElementById('myimage').src='image/logo/logo.png'
+   }
+ 
+ function image(){
+     document.getElementById('myimage').src='image/samantha/samantha.png'
+ }
+
+
+
+
+function fontChange(){
+    document.getElementById('font').style.fontSize="35px";
+}
+
+
+function friend(){
+    document.getElementById('friend-none').style.display="none"
+}
+
+
+function showtext(){
+    document.getElementById('show').style.display='block'
+}
